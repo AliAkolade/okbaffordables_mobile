0acceb52320150ee72c97cc86067f5133457f5b5
Ali Akolade <aliakolade@gmail.com>
2023-03-22 18:30:29 +0100

    start work

---
+++ lib/utils.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OKBColors {
+  static const mainColor = Color.fromRGBO(0, 204, 255, 1);
+}
+
+class OKBStyles {
+  static TextStyle titleText =
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+  static TextStyle bodyText = const TextStyle(fontSize: 14);
+}
--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:okbaffordables/utils.dart";
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -12,6 +13,15 @@ class Welcome extends StatefulWidget {
 class WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: SafeArea(
+            child: Column(children: [
+      Text("Buy your dream house with comfort and ease",
+          textAlign: TextAlign.center, style: OKBStyles.titleText),
+      Text(
+          "We are committed to providing and ensuring a Smooth and Hassle-Free Real Estate Experience",
+          textAlign: TextAlign.center,
+          style: OKBStyles.bodyText)
+    ])));
   }
 }
